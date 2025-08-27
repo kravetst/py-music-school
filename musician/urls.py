912d1603a82b9017b3fbbe029cb9c5d8be883340
kravetst <taraskravetc28@gmail.com>
2025-08-27 15:46:44 +0300

    Complete Musician app: model, serializer, viewset, URLs, and CRUD

--- musician/urls.py
+++ musician/urls.py
@@ -1,3 +1,25 @@
-urlpatterns = []
+from django.urls import path
+from musician.views import MusicianViewSet
+
 
 app_name = "musician"
+
+musician_list = MusicianViewSet.as_view(
+    actions={
+        "get": "list",
+        "post": "create"
+    }
+)
+musician_detail = MusicianViewSet.as_view(
+    {
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy"
+    }
+)
+
+urlpatterns = [
+    path("manage/", musician_list, name="manage-list"),
+    path("manage/<int:pk>/", musician_detail, name="manage-detail"),
+]
